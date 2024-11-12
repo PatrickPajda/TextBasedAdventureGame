f3e4a4b0c8f2d44088950dfb5ba75ee607ceaa1a
PatrickPajda <123835453+PatrickPajda@users.noreply.github.com>
2024-11-12 14:56:03 -0700

    stylecheck

--- include/HauntedHouse.hpp
+++ include/HauntedHouse.hpp
@@ -4,12 +4,12 @@
 #include <string>
 
 class HauntedHouse {
-  public:
-  virtual ~HauntedHouse() = default;
-  virtual void describe() = 0;
- protected:
- HauntedHouse(const std::string& desc) : description(desc) {}
- std::string description;
+    public:
+    virtual ~HauntedHouse() = default;
+    virtual void describe() = 0;
+  protected:
+  HauntedHouse(const std::string& desc) : description(desc) {}
+  std::string description;
 };
 
 #endif //HAUNTEDHOUSE_HPP_INCLUDED
