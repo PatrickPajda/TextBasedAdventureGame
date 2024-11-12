1497280481968ea828f70939704952ec8a2e133e
PatrickPajda <123835453+PatrickPajda@users.noreply.github.com>
2024-11-12 13:11:26 -0700

    Basic Implementation

---
+++ include/HauntedHouse.hpp
@@ -0,0 +1,14 @@
+#ifndef HAUNTEDHOUSE_HPP_INCLUDED
+#define HAUNTEDHOUSE_HPP_INCLUDED
+#include <iostream>
+#include <string>
+
+class HauntedHouse {
+  public:
+  
+  private:
+  std::string description;
+
+};
+
+#endif //HAUNTEDHOUSE_HPP_INCLUDED
--- src/project/main.cpp
+++ src/project/main.cpp
@@ -1,5 +1,34 @@
-// Minimal source file that allows the pipeline to pass.
+#include <iostream>
+#include <string>
 
 int main() {
+    std::cout << "You find yourself in a Haunted House. Right now you are in the living room. What would would like to do?" << std::endl;
+    std::string choice;
+    while (true) {
+        std::cout << "1. Enter the next room." << std::endl;
+        std::cout << "2. Interact. (Describe the room)" << std::endl;
+        std::cout << "3. Pick up object." << std::endl;
+        std::cout << "4. Exit game." <<std::endl;
+        std::cout << "Choice: ";
+        std::getline(std::cin, choice);
+
+        if (choice == "1") {
+            std::cout << "You exit the room into the next room." << std::endl;
+            break;
+        } else if (choice == "2") {
+            std::cout << "You are in the living room. It looks scary." << std::endl;
+            break;
+        } else if (choice == "3") {
+            std::cout << "You picked up an Item." << std::endl;
+            break;
+        } else if (choice == "4") {
+            std::cout << "Exiting Game. Thank-you for playing!" << std::endl;
+            break;
+        } else {
+            std::cout << "Invalid Choice. Please pick valid choice." << std::endl;
+        }
+    }
+
+
     return 0;
 }
