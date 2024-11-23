55ecaf61ad5a5faa19b474023b6ecbcae88a32f7
uzoj2720 <uzoj2720@mailhost.cs.uleth.ca>
2024-11-23 03:37:55 +0000

    update living room hpp

--- include/LivingRoom.hpp
+++ include/LivingRoom.hpp
@@ -20,20 +20,35 @@
  */
 class LivingRoom : public HauntedHouse {
  public:
-    /**
-     * @brief Default constructor for the LivingRoom class.
-     * 
-     * Initializes the LivingRoom with a predefined description.
-     */
-    LivingRoom();
+   /**
+   * @brief Default constructor for the LivingRoom class.
+   * 
+   * Initializes the LivingRoom with a predefined description.
+   */
+   LivingRoom();
 
-    /**
-     * @brief Describes the LivingRoom.
-     * 
-     * Overrides the describe() function from HauntedHouse to print out a description
-     * specific to the living room.
-     */
-    void describe() override;
+   /**
+   * @brief Describes the LivingRoom.
+   * 
+   * Overrides the describe() function from HauntedHouse to print out a description
+   * specific to the living room.
+   */
+   void describe() override;
+
+   /**
+   * @brief Gets options for room
+   * 
+   * Overrides the getActions() function from HauntedHouse to print out options
+   * populates the map of options
+   */
+   std::map<int, std::string> getActions() const override {
+      return {
+         {1, "Talk to the NPC"},
+         {2, "Move to Room 1 (Kitchen)"},
+         {3, "Move to Room 2 (Hallway)"},
+         {4, "Move to Room 3 (Basement)"}
+        };
+    }
 
     /**
      * @brief  Enables interaction with the NPC present in the room
@@ -51,3 +66,19 @@ class LivingRoom : public HauntedHouse {
 
 #endif // LIVINGROOM_HPP_INCLUDED
 
+#include "Room.hpp"
+
+class LivingRoom : public Room {
+public:
+    LivingRoom() : Room("You are in the living room. It looks spooky.") {}
+
+    // Provide actions for the LivingRoom
+    std::map<int, std::string> getActions() const override {
+        return {
+            {1, "Talk to the NPC"},
+            {2, "Move to Room 1 (Kitchen)"},
+            {3, "Move to Room 2 (Hallway)"},
+            {4, "Move to Room 3 (Basement)"}
+        };
+    }
+};
